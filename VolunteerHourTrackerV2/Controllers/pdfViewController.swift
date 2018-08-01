21d25fa0bcb457f014981a1c41f38a51231ba168
Emily Jewik <emilyjewik@gmail.com>
2018-08-01 13:39:00 -0700

    Commit will (not) have pdf share working

--- VolunteerHourTrackerV2/Controllers/pdfViewController.swift
+++ VolunteerHourTrackerV2/Controllers/pdfViewController.swift
@@ -10,7 +10,12 @@ import Foundation
 import SimplePDF
 import UIKit 
 
-class pdfViewController : UIViewController {
+class pdfViewController : UIViewController, UIDocumentInteractionControllerDelegate {
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    let pdfData2 : Data = Data()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,11 +34,103 @@ class pdfViewController : UIViewController {
         
         let pdfData = pdf.generatePDFdata()
         
+       
+        let path = getDocumentsDirectory()
+        
+        print("path is \(path)")
+        
         // save as a local file
-        try? pdfData.writeToFile(path, options: .DataWritingAtomic)
+        //try? pdfData.write( to: path , options: .atomic)
+        
+        do {
+            try pdfData.write( to: path , options: .atomic)
+        }
+        catch {
+            print("catch error",error.localizedDescription)
+        }
+        
+        
+        
     }
     
     
-
+    @IBAction func shareDataButtonTapped(_ sender: Any) {
+        
+//        let activityController = UIActivityViewController(activityItems: [textField.text!],
+//                                                          applicationActivities: nil)
+//        present(activityController, animated: true, completion: nil)
+//        let url = Bundle.main.url(forResource: "file:///Users/emilyjewik/Library/Developer/CoreSimulator/Devices/1F648723-F5E2-428A-9411-C5F01E89D745/data/Containers/Data/Application/A2364309-6D8D-45D0-8B4A-A7F15901286B/Documents/" , withExtension: nil )
+        
+        let url = URL(fileURLWithPath: "file:///Users/emilyjewik/Library/Developer/CoreSimulator/Devices/1F648723-F5E2-428A-9411-C5F01E89D745/data/Containers/Data/Application/A2364309-6D8D-45D0-8B4A-A7F15901286B/Documents/")
+        //guard let url = Bundle.main.url(forResource: "www.example.com", withExtension: nil ) else { return }
+        let controller = UIDocumentInteractionController(url: url)
+        //probably did url incorrectly goddammit
+        controller.delegate = self
+        controller.presentPreview(animated: true)
+        print("should present share...?")
+        
+    }
+    
+    //https://www.youtube.com/watch?v=Nkaoz0ctwvA
+    
+    func documentInteractionControllerViewControllerForPreview(_ controller: UIDocumentInteractionController) -> UIViewController {
+        return self
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+        return documentsDirectory
+    }
+    //https://www.hackingwithswift.com/example-code/system/how-to-find-the-users-documents-directory
+    
     
 }
+
+//class ViewController: UIViewController, UIDocumentInteractionControllerDelegate {
+//
+//    final private let stringWithLink = "Please download this app here in App Store: https://google.com"
+//
+//    @IBAction func shareStringTapped(_ sender: Any) {
+//        let activityController = UIActivityViewController(activityItems: [stringWithLink], applicationActivities: nil)
+//
+//        activityController.completionWithItemsHandler = { (nil, completed, _, error) in
+//            if completed {
+//                print("completed")
+//            } else {
+//                print("cancled")
+//            }
+//        }
+//        present(activityController, animated: true) {
+//            print("presented")
+//        }
+//    }
+//
+//    @IBAction func shareImageTapped(_ sender: Any) {
+//        guard let image = UIImage(named: "img") else { return }
+//        let activityController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+//
+//        activityController.completionWithItemsHandler = { (nil, completed, _, error) in
+//            if completed {
+//                print("completed")
+//            } else {
+//                print("cancled")
+//            }
+//        }
+//        present(activityController, animated: true) {
+//            print("presented")
+//        }
+//    }
+//
+//    @IBAction func sharePDFTapped(_ sender: Any) {
+//        guard let url = Bundle.main.url(forResource: "img", withExtension: "pdf") else { return }
+//        let controller = UIDocumentInteractionController(url: url)
+//        controller.delegate = self
+//        controller.presentPreview(animated: true)
+//    }
+//
+//    func documentInteractionControllerViewControllerForPreview(_ controller: UIDocumentInteractionController) -> UIViewController {
+//        return self
+//    }
+//
+//}
---
+++ VolunteerHourTrackerV2/Helpers/DataProviderHelper.swift
@@ -0,0 +1,23 @@
+//
+//  DataProviderHelper.swift
+//  VolunteerHourTrackerV2
+//
+//  Created by Emily Jewik on 8/1/18.
+//  Copyright © 2018 Emily Jewik. All rights reserved.
+//
+
+import UIKit
+
+//class DataProviderHelper: UIActivityItemProvider {
+//    override func item() -> AnyObject {
+//        self.placeholderItem
+//    }
+//    
+//    override func activityViewController(activityViewController: UIActivityViewController, dataTypeIdentifierForActivityType activityType: String?) -> String {
+//        return kUTTypePDF as String
+//    }
+//    
+//    func activityViewControllerPlaceholderItem(activityViewController: UIActivityViewController) -> AnyObject {
+//        return self.placeholderItem
+//    }
+//}
